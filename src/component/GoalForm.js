c2d54653e6f9011f42ec68c04911b58672fa0387
sonu gupta <sonu.gupta@ldh.01s.in>
2021-07-07 18:53:42 +0530

    ui component

--- src/component/GoalForm.js
+++ src/component/GoalForm.js
@@ -2,24 +2,20 @@ import React from "react";
 
 const GoalForm = () => {
   return (
-
-          <form>
-            <div className="form-group">
-              <label for="exampleInputEmail1">Enter your Goal</label>
-              <input
-                type="text"
-                className="form-control"
-                id="goal"
-                placeholder="Enter Goal"
-              />
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </form>
-        </div>
+    <form>
+      <div className="form-group">
+        <label for="exampleInputEmail1">Enter your Goal</label>
+        <input
+          type="text"
+          className="form-control"
+          id="goal"
+          placeholder="Enter Goal"
+        />
       </div>
-    </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
   );
 };
 
