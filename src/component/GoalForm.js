780a0bea8ab69feea3def5d400df250405777eb8
sonu gupta <sonu.gupta@ldh.01s.in>
2021-07-08 17:49:03 +0530

    validation of goal

--- src/component/GoalForm.js
+++ src/component/GoalForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const GoalForm = (props) => {
   const [addGoal, setAddGoal] = useState("");
+  const [error, setError] = useState("");
 
   const goalChangeHandler = (e) => {
     setAddGoal(e.target.value);
@@ -10,33 +11,54 @@ const GoalForm = (props) => {
   const onSaveHandler = (e) => {
     e.preventDefault();
 
-    const goalFormData = {
-      id: Math.random().toString(),
-      goal: addGoal,
-    };
+    if (validateGoal(addGoal)) {
+      const goalFormData = {
+        id: Math.random().toString(),
+        goal: addGoal,
+      };
 
-    props.singleGoalData(goalFormData);
+      props.singleGoalData(goalFormData);
+    }
 
     setAddGoal("");
   };
 
+  const validateGoal = (e) => {
+    if (addGoal.trim().length === 0) {
+      setError({ message: "Goal can't be empty" });
+      return;
+    }
+    const goalRegex = new RegExp(
+      "^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$"
+    );
+    return true;
+  };
+
   return (
-    <form onSubmit={onSaveHandler}>
-      <div className="form-group">
-        <label htmlFor="exampleInputEmail1">Enter your Goal</label>
-        <input
-          type="text"
-          className="form-control"
-          id="goal"
-          placeholder="Enter Goal"
-          value={addGoal}
-          onChange={goalChangeHandler}
-        />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
+    <>
+      <form onSubmit={onSaveHandler}>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Enter your Goal</label>
+          <input
+            type="text"
+            className="form-control"
+            id="goal"
+            name="goal"
+            placeholder="Enter Goal"
+            value={addGoal}
+            onChange={goalChangeHandler}
+          />
+          {error && (
+            <span className="" style={{ color: "red" }}>
+              {error.message}
+            </span>
+          )}
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </>
   );
 };
 
