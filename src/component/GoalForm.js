721f63ace88d553944785d6d6552932610677fa7
sonu gupta <sonu.gupta@ldh.01s.in>
2021-07-13 18:49:21 +0530

    goal validation modal added

--- src/component/GoalForm.js
+++ src/component/GoalForm.js
@@ -1,37 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import ReactDOM from "react-dom";
 
 const GoalForm = (props) => {
-  const [addGoal, setAddGoal] = useState("");
+  const goalInputref = useRef();
+
   const [error, setError] = useState("");
 
-  const goalChangeHandler = (e) => {
-    setAddGoal(e.target.value);
-  };
+  const goalRegex = new RegExp("^[a-zA-Z]");
 
   const onSaveHandler = (e) => {
+    const addGoal = goalInputref.current.value;
     e.preventDefault();
-
-    if (validateGoal(addGoal)) {
-      const goalFormData = {
-        id: Math.random().toString(),
-        goal: addGoal,
-      };
-
-      props.singleGoalData(goalFormData);
-    }
-
-    setAddGoal("");
-  };
-
-  const validateGoal = (e) => {
     if (addGoal.trim().length === 0) {
       setError({ message: "Goal can't be empty" });
       return;
     }
-    const goalRegex = new RegExp(
-      "^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$"
+    if (!goalRegex.test(addGoal)) {
+      setError({ message: "Goal can't be in number" });
+      return;
+    }
+
+    const goalFormData = {
+      id: Math.random().toString(),
+      goal: addGoal,
+    };
+
+    props.singleGoalData(goalFormData);
+
+    goalInputref.current.value = "";
+  };
+
+  const ErrorModal = () => {
+    return (
+      <div>
+        <div
+          className={
+            "modal fade" + (error !== "" ? " show d-block" : " d-none")
+          }
+          tabIndex="-1"
+          role="dialog"
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Invalid Input</h5>
+                <button
+                  type="button"
+                  className="close"
+                  onClick={() => setError("")}
+                >
+                  <span>&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">{error.message}</div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  onClick={() => setError("")}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
-    return true;
   };
 
   return (
@@ -41,12 +76,11 @@ const GoalForm = (props) => {
           <label htmlFor="exampleInputEmail1">Enter your Goal</label>
           <input
             type="text"
-            className="form-control"
+            className={`form-control ${error && "is-invalid"} `}
             id="goal"
             name="goal"
             placeholder="Enter Goal"
-            value={addGoal}
-            onChange={goalChangeHandler}
+            ref={goalInputref}
           />
           {error && (
             <span className="" style={{ color: "red" }}>
@@ -58,6 +92,10 @@ const GoalForm = (props) => {
           Submit
         </button>
       </form>
+      {ReactDOM.createPortal(
+        <ErrorModal />,
+        document.getElementById("modal-root")
+      )}
     </>
   );
 };
