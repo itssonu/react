39ceaa17703ad080dc76415865df2b79067dd581
sonu gupta <sonu.gupta@ldh.01s.in>
2021-10-26 09:48:38 +0530

    gdf

--- src/layout/WebLayout.js
+++ src/layout/WebLayout.js
@@ -27,9 +27,9 @@ export default function WebLayout(props) {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="fake_url">
+        <Link className="navbar-brand" to="/">
           Navbar
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -43,11 +43,6 @@ export default function WebLayout(props) {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
             {menu.map((v, k) => {
               return (
                 <li className="nav-item active" key={"menuList" + k}>
