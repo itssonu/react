086ed54c0df75b6ab413b8976f0b6cf409e6abac
sonu gupta <sonu.gupta@ldh.01s.in>
2021-11-15 19:05:30 +0530

    sc

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,26 +5,29 @@ import WebLayout from "../layout/WebLayout.js";
 import AdminRoutes from "./AdminRoutes.js";
 import WebRoutes from "./WebRoutes.js";
 
-const Routes = () => {
-  return (
-    <>
-      <Switch>
-        <Route path="/admin">
-          <AdminLayout>
-            <AdminRoutes />
-          </AdminLayout>
-        </Route>
-
-        <Route path="/">
-          <WebLayout>
-            <WebRoutes />
-          </WebLayout>
-        </Route>
-        <Route>
-          <h1>web not found</h1>
-        </Route>
-      </Switch>
-    </>
-  );
+class Routes extends React.Component{
+  render() {
+    return (
+      <>
+        <Switch>
+          <Route path="/admin">
+            <AdminLayout>
+              <AdminRoutes />
+            </AdminLayout>
+          </Route>
+  
+          <Route path="/">
+            <WebLayout>
+              <WebRoutes />
+            </WebLayout>
+          </Route>
+          <Route>
+            <h1>web not found</h1>
+          </Route>
+        </Switch>
+      </>
+    );
+  }
+ 
 };
 export default Routes;
