dae5aeae3d3a0bb128bdbb043f17921ff6d8ba34
sonu gupta <sonu.gupta@ldh.01s.in>
2021-11-15 15:21:12 +0530

    change url api

--- src/http/http.js
+++ src/http/http.js
@@ -7,7 +7,7 @@ import axios from "axios";
 //   "::" +
 //   window.location.port;
 export default axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "http://127.0.0.1:8001/api/",
   headers: {
     "Content-type": "application/json",
     "Access-Control-Allow-Origin": "*",
